257343918895e4cafd48b1eb5b7a9f0c69633f16
M Algah Fattah Illahi <malgahfattahillahi@gmail.com>
2024-03-31 15:21:10 +0700

    fix: sfx

--- src/config/index.ts
+++ src/config/index.ts
@@ -1,4 +1,4 @@
-// export const MINES_PARTY_SERVER_BASE_URL = 'http://localhost:8080';
-// export const MINES_PARTY_SOCKET_SERVER_URL = 'ws://localhost:8080';
-export const MINES_PARTY_SERVER_BASE_URL = 'https://api.mines.aryuuu.com';
-export const MINES_PARTY_SOCKET_SERVER_URL = 'wss://api.mines.aryuuu.com';
+export const MINES_PARTY_SERVER_BASE_URL = 'http://localhost:8080';
+export const MINES_PARTY_SOCKET_SERVER_URL = 'ws://localhost:8080';
+// export const MINES_PARTY_SERVER_BASE_URL = 'https://api.mines.aryuuu.com';
+// export const MINES_PARTY_SOCKET_SERVER_URL = 'wss://api.mines.aryuuu.com';
