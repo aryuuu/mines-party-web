fd2850abebf9edd6f0d4673a5a947996d88fa4a2
M Algah Fattah Illahi <malgahfattahillahi@gmail.com>
2024-05-04 07:32:27 +0700

    fix: audio path

--- src/sfx/index.ts
+++ src/sfx/index.ts
@@ -1,4 +1,4 @@
-export const openCellSfx = new Audio('../../public/audio/zapsplat_foley_paper_sheet_a4_slam_down_by_hand_on_glass_table_001_28576.mp3');
-export const flagCellSfx = new Audio('../../public/audio/zapsplat_food_apple_stab_with_knife_003_57268.mp3');
-export const moveCellSfx = new Audio('../../public/audio/household_2_small_glass_bowls_stack.mp3');
-export const newMessageSfx = new Audio('../../public/audio/zapsplat_food_drink_alcohol_spirit_bottle_tilt_movement_splosh_recorded_over_open_top_003_70138.mp3');
+export const openCellSfx = new Audio('../../audio/zapsplat_foley_paper_sheet_a4_slam_down_by_hand_on_glass_table_001_28576.mp3');
+export const flagCellSfx = new Audio('../../audio/zapsplat_food_apple_stab_with_knife_003_57268.mp3');
+export const moveCellSfx = new Audio('../../audio/household_2_small_glass_bowls_stack.mp3');
+export const newMessageSfx = new Audio('../../audio/zapsplat_food_drink_alcohol_spirit_bottle_tilt_movement_splosh_recorded_over_open_top_003_70138.mp3');
